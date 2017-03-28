c637350857d82274ddf3cc95dd56a730b4441e7e
JavVogt <jav.vogt@gmail.com>
2017-03-28 22:35:26 +1030

    commenting

--- js/sidenav.js
+++ js/sidenav.js
@@ -1,9 +1,9 @@
 function openNav(){
 	document.getElementById("mySidenav").style.width = "250px";
 	document.getElementById("main").style.marginLeft = "250px";
-}
+} // This is the code which runs when the navigation panel is clicked to be opened
 
 function closeNav() {
 	document.getElementById("mySidenav").style.width = "0";
 	document.getElementById("main").style.marginLeft= "0";
-}
+} // This is the code which runs when the navigation panel is clicked to be closed
